063a12a46f016d10170ace4dfb1252e17627a914
prof-rapos <eric.rapos@ontariotechu.ca>
2025-09-03 10:07:18 -0400

    Upload sample code.

---
+++ Section_014/Lecture_1A/MyFirstProgram/src/MyFirstProgram.java
@@ -0,0 +1,14 @@
+public class MyFirstProgram {
+  public static void main(String[] args) {
+    int age = getAge(2000, false);
+    System.out.println("I am " + age + " years old!");
+  }
+
+  public static int getAge(int birthYear, boolean hadBirthday){
+    int age = 2025 - birthYear;
+    if(!hadBirthday) {
+      age--;
+    }
+    return age;
+  }
+}
